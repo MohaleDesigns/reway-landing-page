fee150fd142cbe9ac4164c2d43af935a11e34eaa
TokolohoMohale <150892343+TokolohoMohale@users.noreply.github.com>
2024-07-29 09:45:22 +0200

    fix: removed the test text

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,7 +5,7 @@ function Banner() {
     <div className="bg-lightGrey mb-5">
       <div className="container mx-auto md:flex md:flex-row md:items-center md:justify-between flex flex-col gap-5 items-center justify-center p-5">
         <h4 className="text-darkGrey font-Suez uppercase md:text-sm text-base">
-          Adventure is just around the corner! kabelo
+          Adventure is just around the corner!
         </h4>
 
         <div className="flex gap-1 items-center">
